78d3f3a883aac4e6b498cf703ce507ed206f1a67
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-02-10 20:25:26 -0800

    Add bench (not building) and lens update.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

---
+++ Reflex.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE CPP #-}
+-- | This module exports all of the commonly-used functionality of Reflex; if
+-- you are just getting started with Reflex, this is probably what you want.
+module Reflex
+  ( module X
+  ) where
+
+import Reflex.Basics as X
+import Reflex.EventWriter as X
+import Reflex.Dynamic as X
+#ifdef USE_TEMPLATE_HASKELL
+import Reflex.Dynamic.TH as X
+#endif
+import Reflex.Dynamic.Uniq as X
+import Reflex.DynamicWriter as X
+import Reflex.PerformEvent.Base as X
+import Reflex.PerformEvent.Class as X
+import Reflex.PostBuild.Base as X
+import Reflex.PostBuild.Class as X
+import Reflex.Requester.Base as X
+import Reflex.Requester.Class as X
+import Reflex.Spider as X
+import Reflex.TriggerEvent.Base as X
+import Reflex.TriggerEvent.Class as X
